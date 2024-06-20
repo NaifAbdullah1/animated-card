9236edc87ea49f311eab08ece800759c1ff14c44
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-06-20 17:53:09 +0300

    Configured local access via home network

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // For local access within home's network
+    host: "192.168.100.168", // Home's IP address.
+    port: 3000, // or any other port number
+  },
+});
