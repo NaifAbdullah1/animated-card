b8a7107610411c9cda25bfe86ef5b73949858a4f
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-06-20 12:32:28 +0300

    Animated Card's initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h2 className="card-title">Something Awesome</h2>
+        <p className="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
+          culpa.
+        </p>
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default App;
